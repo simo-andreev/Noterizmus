65d815e33b868ea412199853e0d17ab80d81453e
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2018-11-07 04:12:45 +0200

    feat: Add simple connect to a Mongrel backend impl.

--- app/src/main/java/bg/o/sim/noterizmus/NewNoteFragment.kt
+++ app/src/main/java/bg/o/sim/noterizmus/NewNoteFragment.kt
@@ -1,17 +1,16 @@
 package bg.o.sim.noterizmus
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import bg.o.sim.noterizmus.networking.backendService
+import bg.o.sim.noterizmus.persistance.Note
+import kotlinx.android.synthetic.main.fragment_new_note.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A simple [Fragment] subclass.
@@ -27,5 +26,28 @@ class NewNoteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_new_note, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        title.setText("TITLE TEST")
+        content.setText("CONTENT TEST")
+
+        button_send.setOnClickListener {
+            backendService.saveNote(Note(title.text.toString(), content.text.toString())).enqueue(
+                object : Callback<Note> {
+                    override fun onFailure(call: Call<Note>, t: Throwable) {
+                        title2.setText("Fuck :/")
+                        content2.setText(t.localizedMessage)
+                    }
+
+                    override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                        title2.setText(response.body()?.title)
+                        content2.setText(response.body()?.content)
+                    }
+                }
+            )
+
+        }
+    }
 
 }
--- app/src/main/java/bg/o/sim/noterizmus/networking/BackendConnector.kt
+++ app/src/main/java/bg/o/sim/noterizmus/networking/BackendConnector.kt
@@ -1,1 +1,35 @@
 package bg.o.sim.noterizmus.networking
+
+import android.util.Log
+import bg.o.sim.noterizmus.persistance.Note
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface MongrelService {
+    @GET("notes/all")
+    fun getAllNotes(): Call<List<Note>>
+
+    @POST("notes/save")
+    fun saveNote(@Body note: Note): Call<Note>
+}
+
+val mongrelClient: OkHttpClient = OkHttpClient.Builder()
+    .connectTimeout(30_000, TimeUnit.MILLISECONDS)
+    .readTimeout(30_000, TimeUnit.MILLISECONDS)
+    .addInterceptor(HttpLoggingInterceptor{ Log.d("HTTP LOG" /*TODO*/, "\n $it \n")}.setLevel(HttpLoggingInterceptor.Level.BODY))
+    .build()
+
+val backendService = Retrofit.Builder()
+    .client(mongrelClient)
+    .baseUrl(HttpUrl.Builder().scheme("http").host("192.168.1.7").port(19000).build())
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+    .create(MongrelService::class.java)!!
